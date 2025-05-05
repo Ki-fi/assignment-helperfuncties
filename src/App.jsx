696a1b593248b2f0726a1531536d520867c78baf
Ki-fi <k.meekels@gmail.com>
2025-05-05 12:01:10 +0200

    Uitwerking 2a en b

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import './helpers/calculateSoldTvs.js';
 import calculateSoldTvs from "./helpers/calculateSoldTvs.js";
 import calculatePurchasedTvs from "./helpers/calculatePurchasedTvs.js";
 import calculateToBeSoldTvs from "./helpers/calculateToBeSoldTvs.js";
+import generateTvName from "./helpers/generateTvName.js";
+import generateTvPrice from "./helpers/generateTvPrice.js";
+import {bestSellingTv} from "./constants/inventory.js";
 
 function App() {
   return (
       <>
         <h1>Tech It Easy dashboard</h1>
-        <h3>Verkoopoverzicht</h3>
-        <section className="salesOverview">
+
+        <section>
+            <h3>Verkoopoverzicht</h3>
+        <div className="salesOverview">
           <article className="soldProducts">
             <h4>Aantal verkochte producten</h4>
             <h3>{calculateSoldTvs()}</h3>
@@ -22,7 +27,19 @@ function App() {
             <h4>Aantal te verkopen producten</h4>
             <h3>{calculateToBeSoldTvs()}</h3>
           </article>
+        </div>
         </section>
+          <h3>Best verkochte tv</h3>
+              <article className="bestSoldProduct">
+                  <img src={bestSellingTv.sourceImg} alt="img-bestselling-tv"/>
+                  <div>
+                  <h4>{generateTvName()}</h4>
+                  <h3>{generateTvPrice()}</h3>
+                  <p>
+
+                  </p>
+                  </div>
+              </article>
       </>
   )
 }
