9e022ea51c9664dc053b8578d686ec3fe3d7bdb2
Ki-fi <k.meekels@gmail.com>
2025-05-14 13:54:21 +0200

    Uitwerking 1d t/m 1f

--- src/constants/oefenbestand.js
+++ src/constants/oefenbestand.js
@@ -17,11 +17,22 @@ const tvBrandsAndNames = inventory.map((product) => {
     return `name: ${product.brand} ${product.name}, suitable: ${suitable}`}
 );
 
+const biggerThan65 = inventory.filter((product) => {
+    return product.availableSizes.find((size) => size >= 65)
+});
+// (want array binnen array)
+
+const hasAmbilight = inventory.filter((product) => {
+    return product.options.filter((option) => option.name === "ambilight")
+})
+
 function resultOefenbestand() {
-    console.log(tvNames);
-    console.log(soldOut);
-    console.log(tvInfo);
-    console.log(tvBrandsAndNames)
+    // console.log(tvNames);
+    // console.log(soldOut);
+    // console.log(tvInfo);
+    // console.log(tvBrandsAndNames);
+    // console.log(biggerThan65);
+    console.log(hasAmbilight);
 }
 
 export default resultOefenbestand;
